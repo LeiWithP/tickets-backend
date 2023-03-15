b9e83e7488ec7fd1f2b5e7c1b8c97d54e746291a
LeiWithP <pedro.barrita1029@gmail.com>
2023-03-14 23:58:28 -0600

    Catalogos implemented as choices

--- tickets/models.py
+++ tickets/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from datetime import datetime
-
-PRIORIDAD = [
-    ('1', '1-URGENTE'),
-    ('2', '2-Importante'),
-    ('3', '3-Normal'),
-    ('4', '4-Programado'),
-]
+from tickets import catalogos
 
 class Tickets(models.Model):
     informacion = models.CharField(max_length=250)
@@ -14,7 +8,42 @@ class Tickets(models.Model):
         default=datetime.today
     )
     prioridad = models.CharField(
-        max_length=1,
-        choices=PRIORIDAD,
+        max_length=2,
+        choices=catalogos.PRIORIDAD,
+        default=3,
+    )
+    estado = models.CharField(
+        max_length=2,
+        choices=catalogos.ESTADO,
+        default=3,
+    )
+    uso = models.CharField(
+        max_length=2,
+        choices=catalogos.USO,
+        default=3,
+    )
+    frecuencia = models.CharField(
+        max_length=2,
+        choices=catalogos.FRECUENCIA,
+        default=3,
+    )
+    duracion = models.CharField(
+        max_length=2,
+        choices=catalogos.DURACION,
+        default=3,
+    )
+    medio_origen = models.CharField(
+        max_length=2,
+        choices=catalogos.MEDIO_ORIGEN,
+        default=3,
+    )
+    error = models.CharField(
+        max_length=2,
+        choices=catalogos.ERROR,
+        default=3,
+    )
+    tipo_error = models.CharField(
+        max_length=2,
+        choices=catalogos.TIPO_ERROR,
         default=3,
     )
--- tickets/serializers.py
+++ tickets/serializers.py
@@ -4,5 +4,5 @@ from .models import Tickets
 class TicketsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tickets
-        fields = ('id', 'informacion', 'fecha_solicitud', 'prioridad')
+        fields = ('id', 'informacion', 'fecha_solicitud', 'prioridad', 'estado', 'uso', 'frecuencia', 'duracion', 'medio_origen', 'error', 'tipo_error',)
         read_only_fields = ('fecha_solicitud',)
