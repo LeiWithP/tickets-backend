e9bad18ee464b2c4d59a3ece3a042cd18f6d29d5
LeiWithP <pedro.barrita1029@gmail.com>
2023-03-21 21:05:55 -0600

    register api and path implemented

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.urls import include, path
-from tickets import api
+from tickets import api, views
 #from .api import UserViewSet, GroupViewSet
 #from .api import TicketsViewSet, EmpresasViewSet
 
@@ -11,4 +11,7 @@ router.register('api/groups', api.GroupViewSet)
 router.register('api/tickets', api.TicketsViewSet, 'tickets')
 router.register('api/empresas', api.EmpresasViewSet, 'empresas')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api/register/', views.register_api),
+]
--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.auth import AuthToken
+from .serializers import RegisterSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def register_api(request):
+    serializer = RegisterSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    user = serializer.save()
+    _, token = AuthToken.objects.create(user)
+
+    return Response()
